1f7d9c6ecc7ce208bf57167e8a3944ad6c87986f
UrsZeidler <github@urszeidler.de>
2016-11-06 20:33:59 +0100

    terminate the execution

--- de.urszeidler.eclipse.solidity.laucher.core/src/de/urszeidler/eclipse/solidity/laucher/core/GenerateUml2Solidity.java
+++ de.urszeidler.eclipse.solidity.laucher.core/src/de/urszeidler/eclipse/solidity/laucher/core/GenerateUml2Solidity.java
@@ -14,8 +14,11 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.IProcess;
+import org.eclipse.debug.core.model.IStreamsProxy;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.preference.PreferenceStore;
@@ -28,6 +31,71 @@ import de.urszeidler.eclipse.solidity.util.Uml2Service;
  *
  */
 public class GenerateUml2Solidity extends LaunchConfigurationDelegate {
+
+	private final class IProcessImplementation implements IProcess {
+		private ILaunch launch;
+		private boolean terminated = false;
+		int exitValue = 0;
+		String label;
+
+		public IProcessImplementation(ILaunch launch) {
+			this.launch = launch;
+		}
+
+		@Override
+		public void terminate() throws DebugException {
+			
+		}
+
+		@Override
+		public boolean isTerminated() {
+			return terminated;
+		}
+
+		@Override
+		public boolean canTerminate() {
+			return false;
+		}
+
+		@Override
+		public <T> T getAdapter(Class<T> adapter) {
+			return null;
+		}
+
+		@Override
+		public void setAttribute(String key, String value) {
+			
+		}
+
+		@Override
+		public IStreamsProxy getStreamsProxy() {
+			return null;
+		}
+
+		@Override
+		public ILaunch getLaunch() {
+			return launch;
+		}
+
+		@Override
+		public String getLabel() {
+			return label;
+		}
+
+		@Override
+		public int getExitValue() throws DebugException {
+			return exitValue;
+		}
+
+		@Override
+		public String getAttribute(String key) {
+			return launch.getAttribute(key);
+		}
+
+		public void setTerminated(boolean terminated) {
+			this.terminated = terminated;
+		}
+	}
 
 	public static final String MODEL_URI = "modelUri";
 
@@ -47,7 +115,6 @@ public class GenerateUml2Solidity extends LaunchConfigurationDelegate {
 		final IFile file = (IFile) findMember;
 
 		final ILaunchConfiguration con1 = configuration;
-		//TODO: check out if this is a nice and simple way
 		Uml2Service.setStore(new PreferenceStore(){
 			@Override
 			public String getString(String name) {
@@ -69,16 +136,24 @@ public class GenerateUml2Solidity extends LaunchConfigurationDelegate {
 			}
 		});
 		configuration.getAttributes();
+		IProcessImplementation process = new IProcessImplementation(launch);
 		try {
+			launch.addProcess(process);
+			process.label = "generation working";
 			IContainer target = file.getProject();//ResourcesPlugin.getWorkspace().getRoot();//model.getProject().getFolder(gtarget);
 			GenerateAll generator = new GenerateAll(modelURI, target, new ArrayList<Object>());
 			generator.doGenerate(monitor);
+			process.label = "generation finished.";
 		} catch (IOException e) {
+			process.label = e.getMessage();
+			process.exitValue =1;
 			throw new CoreException(Status.CANCEL_STATUS);
 		} finally {
 			file.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
 		}
-
+		if(monitor!=null)
+			monitor.done();
+		process.setTerminated(true);
 	}
 
 }
