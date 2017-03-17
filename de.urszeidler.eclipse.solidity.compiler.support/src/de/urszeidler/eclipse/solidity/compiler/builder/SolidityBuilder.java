054110abefc63a609059cb740af5d2943368174a
UrsZeidler <github@urszeidler.de>
2017-03-17 17:24:27 +0100

    added compiler markers

--- de.urszeidler.eclipse.solidity.compiler.support/src/de/urszeidler/eclipse/solidity/compiler/builder/SolidityBuilder.java
+++ de.urszeidler.eclipse.solidity.compiler.support/src/de/urszeidler/eclipse/solidity/compiler/builder/SolidityBuilder.java
@@ -7,13 +7,17 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
@@ -55,7 +59,17 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 	}
 
 	public static final String BUILDER_ID = "de.urszeidler.eclipse.solidity.compiler.support.SolidityCompilerBuilder";
+	public static final String SOLIDITY_MARKER_ID = "de.urszeidler.eclipse.solidity.compiler.solidityCodeMarker";
 
+	public static final String ERROR_PARSER = "^([^:]*):(\\p{Digit}*):(\\p{Digit}*):(.*):(.*)";
+	public static final String MESSAGE_SPLITTER = "\\^(-*)\\^";
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IncrementalProjectBuilder#build(int,
+	 * java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
+	 */
 	@SuppressWarnings("rawtypes")
 	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) {
 		try {
@@ -63,12 +77,9 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 				fullBuild(monitor);
 			} else {
 				IResourceDelta delta = getDelta(getProject());
-				if (delta == null) {
-					// fullBuild(monitor);
-				} else {
+				if (delta != null) {
 					final AtomicBoolean ab = new AtomicBoolean(false);
 					delta.accept(new IResourceDeltaVisitor() {
-
 						@Override
 						public boolean visit(IResourceDelta delta) throws CoreException {
 							if (delta.getResource().getType() == IResource.FILE
@@ -89,6 +100,12 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 		return null;
 	}
 
+	/**
+	 * Do a full build.
+	 * 
+	 * @param monitor
+	 * @throws CoreException
+	 */
 	private void fullBuild(IProgressMonitor monitor) throws CoreException {
 		IPreferenceStore store = PreferenceConstants.getPreferenceStore(getProject());
 		String src = store.getString(PreferenceConstants.SOL_SRC_DIRECTORY);
@@ -107,10 +124,10 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 			folder = (IContainer) ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(src));
 		} else
 			folder = getProject().getFolder(src);
-		
-		if(folder==null)
+
+		if (folder == null)
 			return;
-		
+
 		if (!folder.exists())
 			folder.getLocation().toFile().mkdirs();
 
@@ -156,9 +173,11 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 		if (monitor != null)
 			monitor.subTask("compile solidity code:" + files);
 
-		executeCompileSource(store, command, outFile, files, new ArrayList<String>());
+		if (!deleteCompilerMarkers(getProject()))
+			return;
 
-		if (store.getBoolean(PreferenceConstants.ESTIMATE_GAS_COSTS)) {
+		boolean errors = executeCompileSource(store, command, outFile, files, new ArrayList<String>());
+		if (!errors && store.getBoolean(PreferenceConstants.ESTIMATE_GAS_COSTS)) {
 			executeEstimateGas(monitor, store, outPath, files, new ArrayList<String>());
 		}
 
@@ -169,13 +188,16 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 	}
 
 	/**
+	 * Compiles the sources.
+	 * 
 	 * @param store
 	 * @param command
 	 * @param outFile
 	 * @param files
 	 * @param options
+	 * @return
 	 */
-	private void executeCompileSource(IPreferenceStore store, String command, final IFile outFile,
+	private boolean executeCompileSource(IPreferenceStore store, String command, final IFile outFile,
 			final List<String> files, final List<String> options) {
 		options.add(command);
 		options.add("--combined-json");
@@ -184,6 +206,7 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 		if (store.getBoolean(PreferenceConstants.ENABLE_GAS_OPTIMIZE))
 			options.add("--optimize");
 
+		final AtomicBoolean errors = new AtomicBoolean(false);
 		StartCompiler.startCompiler(files, new CompilerCallback() {
 
 			@Override
@@ -200,13 +223,83 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 					} catch (IOException e) {
 						Activator.logError("Error ", e);
 					}
-				} else
-					Activator.logError(error + "  " + options, exception);
+				} else {
+					createMarker(error);
+					errors.set(true);
+				}
 			}
 		}, options);
+		return errors.get();
 	}
 
 	/**
+	 * Delete all the solidity compile markers for the project.
+	 * 
+	 * @param project
+	 * @return
+	 */
+	private boolean deleteCompilerMarkers(IProject project) {
+		try {
+			project.deleteMarkers(SOLIDITY_MARKER_ID, false, IResource.DEPTH_INFINITE);
+			return true;
+		} catch (CoreException e) {
+			Activator.logError("Error cleaning Markers.", e);
+		}
+		return false;
+	}
+
+	/**
+	 * Create the marker from the compiler output.
+	 * 
+	 * @param error
+	 */
+	private void createMarker(String error) {
+		String[] errors = error.trim().split(MESSAGE_SPLITTER);//error.trim().split("\\^");//
+		Pattern pattern = Pattern.compile(ERROR_PARSER, Pattern.DOTALL | Pattern.MULTILINE);
+		IPath location = getProject().getLocation();
+
+		for (String errorMessage : errors) {
+			Matcher matcher = pattern.matcher(errorMessage.trim());
+			if (!matcher.matches())
+				continue;
+
+			String filename = matcher.group(1).trim();
+			String lineNumber = matcher.group(2);
+			String errorType = matcher.group(4).trim();
+			String errorM = matcher.group(5).trim();
+
+			Path path = new Path(filename);
+			if (location.isPrefixOf(path)) {
+				IPath filePath = path.removeFirstSegments(location.segmentCount());
+				IResource resource = getProject().findMember(filePath);
+				if (!resource.exists())
+					continue;
+
+				try {
+					int lineN = Integer.parseInt(lineNumber);
+					IMarker marker = resource.createMarker(SOLIDITY_MARKER_ID);
+					Map<String, Object> attributes = new HashMap<String, Object>();
+
+					attributes.put(IMarker.MESSAGE, errorM);
+					attributes.put(IMarker.LINE_NUMBER, lineN);
+					if ("Error".equalsIgnoreCase(errorType))
+						attributes.put(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+					else if ("Warning".equalsIgnoreCase(errorType))
+						attributes.put(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
+					else
+						attributes.put(IMarker.SEVERITY, IMarker.SEVERITY_INFO);
+
+					marker.setAttributes(attributes);
+				} catch (Exception e) {
+					Activator.logError("Error creating marker for: " + filename, e);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Estimate the gas cost.
+	 * 
 	 * @param monitor
 	 * @param store
 	 * @param outPath
@@ -245,5 +338,4 @@ public class SolidityBuilder extends IncrementalProjectBuilder {
 			}
 		}, options);
 	}
-
 }
