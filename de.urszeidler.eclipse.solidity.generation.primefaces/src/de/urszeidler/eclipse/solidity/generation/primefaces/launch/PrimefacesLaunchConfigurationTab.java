3fbd087890b9df61e5f67b8b6050fa561b440022
Urs Zeidler <uzeidler@sernet.de>
2017-08-27 23:42:43 +0200

    add application properties

--- de.urszeidler.eclipse.solidity.generation.primefaces/src/de/urszeidler/eclipse/solidity/generation/primefaces/launch/PrimefacesLaunchConfigurationTab.java
+++ de.urszeidler.eclipse.solidity.generation.primefaces/src/de/urszeidler/eclipse/solidity/generation/primefaces/launch/PrimefacesLaunchConfigurationTab.java
@@ -26,9 +26,7 @@ import de.urszeidler.eclipse.solidity.laucher.ui.AbstractUml2SolidityLaunchConfi
  * @author urszeidler
  *
  */
-public class PrimefacesLaunchConfigurationTab
-		extends 
-		AbstractUml2SolidityLaunchConfigurationTab {
+public class PrimefacesLaunchConfigurationTab extends AbstractUml2SolidityLaunchConfigurationTab {
 
 	private static final String DEFAULT_TABLIB_TARGET = "src/main/resources/META-INF/resources/taglib";
 	private static final String GENERATE_TAG_LIBS = "de.urszeidler.eclipse.solidity.generation.primefaces.taglib.generator";
@@ -37,12 +35,16 @@ public class PrimefacesLaunchConfigurationTab
 	private static final String GENERATE_CONTRACT_BEANS_TARGET = "de.urszeidler.eclipse.solidity.generation.primefaces.contractBean.target";
 	private static final String DEFAULT_CONTRACT_BEANS_TARGET = "src/";
 	private static final String BASE_PACKAGE = "BASE_PACKAGE";
+	private static final String GENERATE_APPLICATION_FILES = "de.urszeidler.eclipse.solidity.generation.primefaces.GenerateApplicationFiles";
+	private static final String GENERATE_APPLICATION_FILES_TARGET = "de.urszeidler.eclipse.solidity.generation.primefaces.GenerateApplicationFiles.target";
+	private static final String DEFAULT_GENERATE_APPLICATION_FILES_TARGET = "src/main";
 	private Button btnGenerateTagLibs;
 	private Text generateTagLibTargetText;
 	private Button btnGenerateContractBeans;
 	private Text generateContractBeansTargetText;
-	private Label lblNewLabel_1;
 	private Text basePackageText;
+	private Button btnGenerateApplicationFiles;
+	private Text generateApplicationTargetText;
 
 	/**
 	 * @wbp.parser.entryPoint
@@ -52,12 +54,12 @@ public class PrimefacesLaunchConfigurationTab
 		Composite mainComposite = new Composite(parent, SWT.NONE);
 		mainComposite.setLayout(new GridLayout(1, true));
 		setControl(mainComposite);
-		
+
 		Group grpPrimefaces = new Group(mainComposite, SWT.NONE);
 		grpPrimefaces.setText("primefaces jsf components");
 		grpPrimefaces.setLayout(new GridLayout(3, false));
 		grpPrimefaces.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
+
 		btnGenerateTagLibs = new Button(grpPrimefaces, SWT.CHECK);
 		btnGenerateTagLibs.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
 		btnGenerateTagLibs.addSelectionListener(new SelectionAdapter() {
@@ -68,10 +70,10 @@ public class PrimefacesLaunchConfigurationTab
 		});
 		btnGenerateTagLibs.setText("generate taglibs");
 		new Label(grpPrimefaces, SWT.NONE);
-		
+
 		Label lblNewLabel = new Label(grpPrimefaces, SWT.NONE);
 		lblNewLabel.setText("taglib target");
-		
+
 		generateTagLibTargetText = new Text(grpPrimefaces, SWT.BORDER);
 		generateTagLibTargetText.addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent e) {
@@ -79,7 +81,7 @@ public class PrimefacesLaunchConfigurationTab
 			}
 		});
 		generateTagLibTargetText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
+
 		Button btnNewButton = new Button(grpPrimefaces, SWT.NONE);
 		btnNewButton.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -89,8 +91,13 @@ public class PrimefacesLaunchConfigurationTab
 		});
 		btnNewButton.setText("select");
 
-		btnGenerateContractBeans = new Button(grpPrimefaces, SWT.CHECK);
-		btnGenerateContractBeans.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		Group grpContractBeans = new Group(mainComposite, SWT.NONE);
+		grpContractBeans.setText("contract Beans");
+		grpContractBeans.setLayout(new GridLayout(3, false));
+		grpContractBeans.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+
+		btnGenerateContractBeans = new Button(grpContractBeans, SWT.CHECK);
+		btnGenerateContractBeans.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
 		btnGenerateContractBeans.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -98,20 +105,19 @@ public class PrimefacesLaunchConfigurationTab
 			}
 		});
 		btnGenerateContractBeans.setText("generate contract beans");
-		new Label(grpPrimefaces, SWT.NONE);
-		
-		Label lblNewLabel1 = new Label(grpPrimefaces, SWT.NONE);
-		lblNewLabel1.setText("contract beans target target");
-		
-		generateContractBeansTargetText = new Text(grpPrimefaces, SWT.BORDER);
+
+		Label lblNewLabel1 = new Label(grpContractBeans, SWT.NONE);
+		lblNewLabel1.setText("contract beans target");
+
+		generateContractBeansTargetText = new Text(grpContractBeans, SWT.BORDER);
+		generateContractBeansTargetText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		generateContractBeansTargetText.addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent e) {
 				validatePage();
 			}
 		});
-		generateContractBeansTargetText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
-		btnNewButton = new Button(grpPrimefaces, SWT.NONE);
+
+		btnNewButton = new Button(grpContractBeans, SWT.NONE);
 		btnNewButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -119,14 +125,45 @@ public class PrimefacesLaunchConfigurationTab
 			}
 		});
 		btnNewButton.setText("select");
-		
-		lblNewLabel_1 = new Label(grpPrimefaces, SWT.NONE);
-		lblNewLabel_1.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+
+		Label lblNewLabel_1 = new Label(grpContractBeans, SWT.NONE);
+		lblNewLabel_1.setSize(86, 20);
 		lblNewLabel_1.setText("base package");
-		
-		basePackageText = new Text(grpPrimefaces, SWT.BORDER);
+
+		basePackageText = new Text(grpContractBeans, SWT.BORDER);
 		basePackageText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		new Label(grpPrimefaces, SWT.NONE);
+		new Label(grpContractBeans, SWT.NONE);
+
+		Group grpApplicationFiles = new Group(mainComposite, SWT.NONE);
+		grpApplicationFiles.setLayout(new GridLayout(3, false));
+		grpApplicationFiles.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		grpApplicationFiles.setText("Application Files");
+
+		btnGenerateApplicationFiles = new Button(grpApplicationFiles, SWT.CHECK);
+		btnGenerateApplicationFiles.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
+		btnGenerateApplicationFiles.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				validatePage();
+			}
+		});
+		btnGenerateApplicationFiles.setText("generate Application files");
+
+		Label lblNewLabel_2 = new Label(grpApplicationFiles, SWT.NONE);
+		lblNewLabel_2.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNewLabel_2.setText("target");
+
+		generateApplicationTargetText = new Text(grpApplicationFiles, SWT.BORDER);
+		generateApplicationTargetText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		Button btnNewButton_1 = new Button(grpApplicationFiles, SWT.NONE);
+		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				handleChooseContainer(generateApplicationTargetText, "select target container");
+			}
+		});
+		btnNewButton_1.setText("select");
 	}
 
 	@Override
@@ -138,10 +175,15 @@ public class PrimefacesLaunchConfigurationTab
 	public void initializeFrom(ILaunchConfiguration configuration) {
 		try {
 			btnGenerateTagLibs.setSelection(configuration.getAttribute(GENERATE_TAG_LIBS, false));
-			generateTagLibTargetText.setText(configuration.getAttribute(GENERATE_TAG_LIBS_TARGET, DEFAULT_TABLIB_TARGET));
+			generateTagLibTargetText
+					.setText(configuration.getAttribute(GENERATE_TAG_LIBS_TARGET, DEFAULT_TABLIB_TARGET));
 			btnGenerateContractBeans.setSelection(configuration.getAttribute(GENERATE_CONTRACT_BEANS, false));
-			generateContractBeansTargetText.setText(configuration.getAttribute(GENERATE_CONTRACT_BEANS_TARGET, DEFAULT_CONTRACT_BEANS_TARGET));
+			generateContractBeansTargetText
+					.setText(configuration.getAttribute(GENERATE_CONTRACT_BEANS_TARGET, DEFAULT_CONTRACT_BEANS_TARGET));
 			basePackageText.setText(configuration.getAttribute(BASE_PACKAGE, ""));
+			btnGenerateApplicationFiles.setSelection(configuration.getAttribute(GENERATE_APPLICATION_FILES, false));
+			generateApplicationTargetText.setText(configuration.getAttribute(GENERATE_APPLICATION_FILES_TARGET,
+					DEFAULT_GENERATE_APPLICATION_FILES_TARGET));
 		} catch (CoreException e) {
 			Activator.logError("Error while initialize launch config.", e);
 		}
@@ -154,6 +196,8 @@ public class PrimefacesLaunchConfigurationTab
 		configuration.setAttribute(GENERATE_CONTRACT_BEANS, btnGenerateContractBeans.getSelection());
 		configuration.setAttribute(GENERATE_CONTRACT_BEANS_TARGET, generateContractBeansTargetText.getText());
 		configuration.setAttribute(BASE_PACKAGE, basePackageText.getText());
+		configuration.setAttribute(GENERATE_APPLICATION_FILES, btnGenerateApplicationFiles.getSelection());
+		configuration.setAttribute(GENERATE_APPLICATION_FILES_TARGET, generateApplicationTargetText.getText());
 	}
 
 	@Override
@@ -163,6 +207,8 @@ public class PrimefacesLaunchConfigurationTab
 		configuration.setAttribute(GENERATE_CONTRACT_BEANS, false);
 		configuration.setAttribute(GENERATE_CONTRACT_BEANS_TARGET, DEFAULT_CONTRACT_BEANS_TARGET);
 		configuration.setAttribute(BASE_PACKAGE, "");
+		configuration.setAttribute(GENERATE_APPLICATION_FILES, false);
+		configuration.setAttribute(GENERATE_APPLICATION_FILES_TARGET, DEFAULT_GENERATE_APPLICATION_FILES_TARGET);
 	}
 
 }
